2164c49bf8765c4473e243aae703aafe82e50681
JuanCardona16 <jcardonabageth@gmail.com>
2025-06-29 11:08:49 -0500

    refactor(user): remove debug console logs in update user endpoint

--- src/features/user/controller/user.controllers.ts
+++ src/features/user/controller/user.controllers.ts
@@ -45,9 +45,6 @@ class UserControllers {
     const { uuid } = req.params;
     const { user } = req.body;
 
-    console.log("uuid: ", uuid);
-    console.log("user: ", user);
-
     if (!uuid || !user) {
       return next(CustomError(400, 'Bad request'));
     }
